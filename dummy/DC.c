a6cf9af311514ab924523e083b7a1b92a2aeafe0
WhAnci <leeshiny7@icloud.com>
2025-11-26 15:15:28 +0900

    Update DC.c

--- dummy/DC.c
+++ dummy/DC.c
@@ -1,11 +1,11 @@
 void setup()
 {
-  pinMode(11, OUTPUT);
+  pinMode(11, OUTPUT); // DC모터
 }
 
 void loop()
 {
-  int inputValue = analogRead(A0);
+  int inputValue = analogRead(A0); // 가변저항
   int convertedValue = map(inputValue,0,1023,0,255);
   analogWrite(11,convertedValue);
   delay(100);
